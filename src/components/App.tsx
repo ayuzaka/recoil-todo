a2610115f171125ff4b02f5e0821636cf37a89da
ayuzaka <ayuzaka.f@gmail.com>
2021-04-03 14:54:14 +0900

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import { VFC } from 'react'
+
+const Component: VFC = () => <h1>Hello World</h1>
+
+export const App = Component
---
+++ webpack.prod.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+
+const src = path.resolve(__dirname, 'src')
+const template = path.resolve(__dirname, 'template')
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: `${template}/development.html`,
+  filename: 'index.html',
+  showErrors: true,
+})
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-cheap-module-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        include: src,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                jsx: 'react-jsxdev',
+              },
+            },
+          },
+        ],
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        include: src,
+        use: [
+          {
+            loader: 'source-map-loader',
+          },
+        ],
+      },
+    ],
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+  },
+
+  plugins: [htmlWebpackPlugin],
+})
