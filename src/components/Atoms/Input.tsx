2d59d20dbf12f01bdc7069a8eb01bc0494286484
ayuzaka <ayuzaka.f@gmail.com>
2021-04-04 19:30:10 +0900

    add input component

---
+++ src/components/Atoms/Input.tsx
@@ -0,0 +1,14 @@
+import { DetailedHTMLProps, InputHTMLAttributes, forwardRef } from 'react'
+import styled from '@emotion/styled'
+
+type Props = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+const Component = forwardRef<HTMLInputElement, Props>((props, ref) => <input {...props} ref={ref} />)
+
+const StyledComponent = styled(Component)`
+  padding: 0.25em;
+  border: 1px solid #000;
+  border-radius: 0.25em;
+`
+
+export const Input = StyledComponent
